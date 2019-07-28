786e4b71252e3af504553ee59f1f02b8a89d0a52
Samet Girginer <10145948+sametgirginer@users.noreply.github.com>
2019-07-28 06:07:29 +0300

    add

---
+++ Blog/Filters/YetkiKontrol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Filters
+{
+    public class Yetki
+    {
+        private static Blog.DAL.BlogEntities db = new DAL.BlogEntities();
+
+        public static bool Kontrol(int id)
+        {
+            var UyeID = db.Uye.Find(id);
+
+            if (UyeID.Yetki == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
